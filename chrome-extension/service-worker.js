c3f6d9c79d9a4e98d3d9d2115b76b9de39851757
ClimenteA <climente.alin@gmail.com>
2024-04-09 15:53:06 +0300

    feat: notification with sound

--- chrome-extension/service-worker.js
+++ chrome-extension/service-worker.js
@@ -5,8 +5,14 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
     await chrome.notifications.create(alarm.name, {
         type: "basic",
         title: alarm.name,
-        message: "You got some new important events. Checkout extension.",
-        iconUrl: chrome.runtime.getURL("icons/notification.jpg")
+        message: "You got some new important events.",
+        iconUrl: chrome.runtime.getURL("static/notification.jpg")
+    })
+
+    await chrome.offscreen.createDocument({
+        url: chrome.runtime.getURL('static/audio.html'),
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: 'notification',
     })
 
 })
